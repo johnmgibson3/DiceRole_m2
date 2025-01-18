168e2e237463c774b6da1c2f290966642458338c
John Gibson <johnmgibson3@gmail.com>
2025-01-17 20:53:42 -0700

    Initial commit

---
+++ DiceRole/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+ 
+ Ask user how many rolls they would like
+ 
+ For the number of times they said:
+        Generate a random number 1-6
+        Generate a second random number 1-6
+        Add two numbers together and store the results
+        Go update the counter of each number
+   
+ For each of the numbers:   
+        Print the number
+        Calculate the percentage of the times it was rolled
+        Print one asterisk for each percent it was rolled
+ 
+ */
+
+Console.WriteLine("Hello, World!");
